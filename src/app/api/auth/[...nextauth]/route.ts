9de8ba4a57c8093b344772351a7077e304f25c7e
unknown <ngocnguyenluong89@gmail.com>
2026-05-30 08:55:26 +0700

    fix: nextauth route handler signature for nextjs 15

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,3 @@
 import { handlers } from "@/auth"
 
-const { GET: AuthGET, POST: AuthPOST } = handlers;
-
-export async function GET(req: any, ctx: any) {
-  return AuthGET(req, ctx);
-}
-
-export async function POST(req: any, ctx: any) {
-  return AuthPOST(req, ctx);
-}
+export const { GET, POST } = handlers;
